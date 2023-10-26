8c66a8cac18f9450f943ce86d4575ac2c781910e
Jay Chauhan <142774567+SF-dev1@users.noreply.github.com>
2023-10-26 17:52:28 +0530

    Login Functionality

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // ---------------------------------------------------------------------------------------------------------------------------
 // uncomment the following lines to display errors
 // ---------------------------------------------------------------------------------------------------------------------------
@@ -7,16 +7,23 @@
 // echo '<pre>';
 // ---------------------------------------------------------------------------------------------------------------------------
 
+session_start();
+if (($_SESSION["login"] != true)) {
+  header("Location: login.php");
+  exit();
+}
+
 require ".env.php";
 require ".env.table.php";
 require "includes/mysqli-class/MysqliDb.php";
 
-$db = new MysqliDb (DB_HOST, DB_USER, DB_PASS, DB_PREFIX . DB_NAME);
+$db = new MysqliDb(DB_HOST, DB_USER, DB_PASS, DB_PREFIX . DB_NAME);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -35,42 +42,45 @@ $db = new MysqliDb (DB_HOST, DB_USER, DB_PASS, DB_PREFIX . DB_NAME);
   <link rel="stylesheet" href="../../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
   <link rel="stylesheet" href="../../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
 </head>
+
 <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-<div class="wrapper">
+  <div class="wrapper">
 
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-dark">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-    </ul>
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-dark">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+      </ul>
 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      <!-- Navbar Search -->
-      <li class="nav-item">
-        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-          <i class="fas fa-search"></i>
-        </a>
-        <div class="navbar-search-block">
-          <form class="form-inline">
-            <div class="input-group input-group-sm">
-              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-              <div class="input-group-append">
-                <button class="btn btn-navbar" type="submit">
-                  <i class="fas fa-search"></i>
-                </button>
-                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                  <i class="fas fa-times"></i>
-                </button>
+      <!-- Right navbar links -->
+      <ul class="navbar-nav ml-auto">
+        <!-- Navbar Search -->
+        <li class="nav-item">
+          <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+            <i class="fas fa-search"></i>
+          </a>
+          <div class="navbar-search-block">
+            <form class="form-inline">
+              <div class="input-group input-group-sm">
+                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                  <button class="btn btn-navbar" type="submit">
+                    <i class="fas fa-search"></i>
+                  </button>
+                  <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
               </div>
-            </div>
-          </form>
-        </div>
-      </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
-  
+            </form>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a href="logout.php"><button type="button" class="btn btn-block btn-outline-danger">Logout</button></a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.navbar -->
---
+++ login.php
@@ -0,0 +1,107 @@
+<?php
+
+session_start();
+if ($_SESSION["login"] == true) {
+    header("Location: index");
+    exit();
+}
+
+if ($_POST["login"]) {
+    $email = $_POST["email"];
+    $password = sha1($_POST["password"]);
+
+   
+
+    if ($email == "jay.chauhan3042@gmail.com" && $password == "b7be3b987b524ee063e2b78d5039b88fbe12fa07") {
+        $_SESSION["login"] = true;
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "<script>alert('Email or password is incorrect!');</script>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Contact Details | Log in</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="index"><b>Contact</b>Details</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Sign in to get detials</p>
+
+      <form method="post">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Email" name="email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password" name="password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <!-- /.col -->
+          <div class="col">
+            <button type="submit" class="btn btn-primary btn-block w-100" name="login" value="1">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <div class="social-auth-links text-center mb-3">
+        <p>- OR -</p>
+        <a href="#" class="btn btn-block btn-primary disabled">
+          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook (comming soon)
+        </a>
+        <a href="#" class="btn btn-block btn-danger disabled">
+          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+ (comming soon)
+        </a>
+      </div>
+      <!-- /.social-auth-links -->
+
+      <p class="mb-1">
+        <a href="">I forgot my password</a>
+      </p>
+      <p class="mb-0">
+        <a href="" class="text-center">Register a new membership</a>
+      </p>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>
