5c0f548dea8fec6a052a99388c5c148504255d6c
Jay Chauhan <142774567+SF-dev1@users.noreply.github.com>
2023-10-30 16:17:39 +0530

    .

--- footer.php
+++ footer.php
@@ -29,18 +29,18 @@
 <script src="plugins/chart.js/Chart.min.js"></script>
 
 <!-- DataTables  & Plugins -->
-<script src="plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="plugins/jszip/jszip.min.js"></script>
-<script src="plugins/pdfmake/pdfmake.min.js"></script>
-<script src="plugins/pdfmake/vfs_fonts.js"></script>
-<script src="plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script src="<?= dirname(__FILE__)?>plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= dirname(__FILE__)?>plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?= dirname(__FILE__)?>plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?= dirname(__FILE__)?>plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="<?= dirname(__FILE__)?>plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="<?= dirname(__FILE__)?>plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="<?= dirname(__FILE__)?>plugins/jszip/jszip.min.js"></script>
+<script src="<?= dirname(__FILE__)?>plugins/pdfmake/pdfmake.min.js"></script>
+<script src="<?= dirname(__FILE__)?>plugins/pdfmake/vfs_fonts.js"></script>
+<script src="<?= dirname(__FILE__)?>plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="<?= dirname(__FILE__)?>plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="<?= dirname(__FILE__)?>plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
 <!-- jQuery -->
 <script src="plugins/jquery/jquery.min.js"></script>
@@ -60,15 +60,6 @@
       "responsive": true, "lengthChange": false, "autoWidth": false,
       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "responsive": true,
-    });
   });
 </script>
 <script>
