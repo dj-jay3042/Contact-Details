d322fc1a6ebba08552b4f4a2c26753048c46aec9
Jay Chauhan <142774567+SF-dev1@users.noreply.github.com>
2023-10-30 16:34:39 +0530

    .

--- footer.php
+++ footer.php
@@ -29,29 +29,23 @@
 <script src="plugins/chart.js/Chart.min.js"></script>
 
 <!-- DataTables  & Plugins -->
-<script src="<?= dirname(__FILE__)?>/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="<?= dirname(__FILE__)?>/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="<?= dirname(__FILE__)?>/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="<?= dirname(__FILE__)?>/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="<?= dirname(__FILE__)?>/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="<?= dirname(__FILE__)?>/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="<?= dirname(__FILE__)?>/plugins/jszip/jszip.min.js"></script>
-<script src="<?= dirname(__FILE__)?>/plugins/pdfmake/pdfmake.min.js"></script>
-<script src="<?= dirname(__FILE__)?>/plugins/pdfmake/vfs_fonts.js"></script>
-<script src="<?= dirname(__FILE__)?>/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="<?= dirname(__FILE__)?>/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="<?= dirname(__FILE__)?>/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script src="plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="plugins/jszip/jszip.min.js"></script>
+<script src="plugins/pdfmake/pdfmake.min.js"></script>
+<script src="plugins/pdfmake/vfs_fonts.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- SweetAlert2 -->
 <script src="plugins/sweetalert2/sweetalert2.min.js"></script>
 <!-- Toastr -->
 <script src="plugins/toastr/toastr.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
 <!-- Summernote -->
 <script src="plugins/summernote/summernote-bs4.min.js"></script>
 <script>
